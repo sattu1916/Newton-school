00067113de1ae098dc3112f68cfcd82885687663
Satyendra Yadav <107065103+sattu1916@users.noreply.github.com>
2022-06-28 00:03:58 +0530

    Create Area of square.cpp

---
+++ Basic of Programming Java Programming/Area of square.cpp
@@ -0,0 +1,15 @@
+/**
+ *    author:  Satyendra Yadav
+**/
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	int n;
+	cin>>n;
+
+	cout<<n*n;
+	return 0;
+}
