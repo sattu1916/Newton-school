f4b01dec4a848beaebb439a5c2f375e2fc16655b
Satyendra Yadav <107065103+sattu1916@users.noreply.github.com>
2022-06-28 00:09:37 +0530

    Create String Typecast.cpp

---
+++ Java Programming Syntax (Post Class)/String Typecast.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  Satyendra Yadav
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	// Your code here
+  int n;
+  cin>>n;
+  if(n){
+    cout<<"Nice Job";
+  }
+  else{
+     cout<<"Wrong answer";
+  }
+	return 0;
+}
